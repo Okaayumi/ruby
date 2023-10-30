6c436dc0990d3a9a3605818afe644037d5a8d563
EC2 Default User <ec2-user@ip-172-31-28-131.ap-southeast-2.compute.internal>
2023-10-30 15:29:10 +0000

    submit

--- memo.rb
+++ memo.rb
@@ -12,13 +12,23 @@ require "csv"
     puts "メモしたい内容を記入してください"
     puts "終了後、Ctrl + D　を押します。"
     
-    memo = STDIN.read
+    memo = STDIN.read.chomp
     
     CSV.open("#{file_name}.csv","w") do |csv|
         csv<< ["#{memo}"]
     end
- 
+    
+  elsif memo_type == 2
+   puts "既存のファイルを入力してください"
+file_name = gets.chomp.chomp
+puts "追記したい内容を記入してください"
+puts "終了後、Ctrl + D　を押します。"
+memo = STDIN.read
+CSV.open("#{file_name}.csv","a") do |csv|#aで既存のファイルを追記モードで呼び出し
+csv << ["#{memo}"]
+end
   else
-　　puts "エラーが発生しました"
+   puts "1または2以外の文字が入力されました"
   end
+#23行目以降半角スペースをメソッドと認識していまいエラーとなるので詰めて記載した。
     
